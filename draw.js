dcb1c03b3b450c95327a7d9d81816278848046ab
536uriel <31188161+536uriel@users.noreply.github.com>
2023-11-22 12:30:37 +0200

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,22 @@
+export function drawRotation(r,...arr){
+    ctx.save()
+    ctx.translate(300,300)
+    ctx.rotate(d)
+    d -= r
+    arr.forEach((s) =>{
+        
+    s.x -= 300
+    s.y -= 300
+
+    s.draw()
+
+    s.x += 300
+    s.y += 300
+    
+    })
+
+
+    
+    ctx.translate(-300,300)
+    ctx.restore()
+}
